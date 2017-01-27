c9c0d545e59940958f7f609581b750aa3ff31004
Jonathan Reisdorf <fishlevel@crossnode.org>
2017-01-27 11:24:43 +0100

    use strict

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-require('util');
-const exec = require('child_process').exec;
+module.exports = (function() {
+  'use strict';
 
-class OmegaOled {
+  require('util');
+  const exec = require('child_process').exec;
+
+  class OmegaOled {
     constructor() {
-        this._chainMode = false;
-        this._commandChain = [];
+      this._chainMode = false;
+      this._commandChain = [];
     }
 
     /**
@@ -13,55 +16,55 @@ class OmegaOled {
      * @param isExecuteChain
      */
     _executeCommand(command, isExecuteChain = false) {
-        if (this._chainMode && !isExecuteChain) {
-            return this._commandChain.push(command);
-        }
+      if (this._chainMode && !isExecuteChain) {
+        return this._commandChain.push(command);
+      }
 
-        return exec('oled-exp ' + command);
+      return exec('oled-exp ' + command);
     }
 
     init() {
-        return this._executeCommand('-i');
+      return this._executeCommand('-i');
     }
 
     clear() {
-        return this._executeCommand('-c');
+      return this._executeCommand('-c');
     }
 
     power(on = true) {
-        return this._executeCommand('power ' + (on ? 'on' : 'off'));
+      return this._executeCommand('power ' + (on ? 'on' : 'off'));
     }
 
     invert(on = true) {
-        return this._executeCommand('invert ' + (on ? 'on' : 'off'));
+      return this._executeCommand('invert ' + (on ? 'on' : 'off'));
     }
 
     dim(on = true) {
-        return this._executeCommand('dim ' + (on ? 'on' : 'off'));
+      return this._executeCommand('dim ' + (on ? 'on' : 'off'));
     }
 
     cursor(row, column) {
-        return this._executeCommand('cursor ' + [row, column].join(','));
+      return this._executeCommand('cursor ' + [row, column].join(','));
     }
 
     cursorPixel(row, pixel) {
-        return this._executeCommand('cursorPixel ' + [row, pixel].join(','));
+      return this._executeCommand('cursorPixel ' + [row, pixel].join(','));
     }
 
     write(string) {
-        return this._executeCommand('write ' + string);
+      return this._executeCommand('write ' + string);
     }
 
     writeByte(byte) {
-        return this._executeCommand('writeByte ' + byte);
+      return this._executeCommand('writeByte ' + byte);
     }
 
     scroll(direction) {
-        return this._executeCommand('scroll ' + direction);
+      return this._executeCommand('scroll ' + direction);
     }
 
     draw(imageFile) {
-        return this._executeCommand('draw ' + imageFile);
+      return this._executeCommand('draw ' + imageFile);
     }
 
     /**
@@ -70,18 +73,19 @@ class OmegaOled {
      * and then execute all commands in one call
      * @param on
      */
-    chainMode(on) {
-        if (typeof on === 'undefined') {
-            return this._chainMode;
-        }
+      chainMode(on) {
+      if (typeof on === 'undefined') {
+        return this._chainMode;
+      }
 
-        this._chainMode = on;
+      this._chainMode = on;
     }
 
     executeChain() {
-        this._executeCommand(this._commandChain.join(' '), true);
-        this._commandChain = [];
+      this._executeCommand(this._commandChain.join(' '), true);
+      this._commandChain = [];
     }
-}
+  }
 
-module.exports = (new OmegaOled());
+  return new OmegaOled();
+});
